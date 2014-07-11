6f8bbdba4222857cd7a6c4d1016fbc8f15d5bf79
Julian Monono <julian.monono@gmail.com>
2014-07-11 17:30:39 +0100

    Initial StringCalculator commit

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        [TestMethod]
+        public void AddZeroNumbersReturnsZero()
+        {
+            // Arrange
+            int expected = 0;
+            var calculator = new StringCalculator();
+            string numbers = String.Empty;
+
+            // Act
+            int actual = calculator.Add(numbersToAdd: numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddOneNumberReturnsTheNumber()
+        {
+            // Arrange
+            var expected = 4;
+            var calculator = new StringCalculator();
+            string numbers = expected.ToString();
+
+            // Act
+            int actual = calculator.Add(numbersToAdd: numbers);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTwoCommaThreeNumbersReturnsFive()
+        {
+            // Arrange
+            var expected = 5;
+            var calculator = new StringCalculator();
+            string numbers = "2,3";
+
+            // Act
+            int actual = calculator.Add(numbersToAdd: numbers);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddFourCommaSevenNumbersReturnsEleven()
+        {
+            // Arrange
+            var expected = 11;
+            var calculator = new StringCalculator();
+            string numbers = "4,7";
+
+            // Act
+            int actual = calculator.Add(numbersToAdd: numbers);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTwoCommaThreeCommaFourNumbersReturnsNine()
+        {
+            // Arrange
+            var expected = 9;
+            var calculator = new StringCalculator();
+            string numbers = "2,3,4";
+
+            // Act
+            int actual = calculator.Add(numbersToAdd: numbers);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTwoNewlineThreeNumbersReturnsFive()
+        {
+            // Arrange
+            var expected = 5;
+            var calculator = new StringCalculator();
+            string numbers = String.Format("2{0}3", Environment.NewLine);
+
+            // Act
+            int actual = calculator.Add(numbersToAdd: numbers);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTwoNewlineThreeCommaFourNumbersReturnsNine()
+        {
+            // Arrange
+            var expected = 9;
+            var calculator = new StringCalculator();
+            string numbers = String.Format("2{0}3{0}4", Environment.NewLine, ",");
+
+            // Act
+            int actual = calculator.Add(numbersToAdd: numbers);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbersToAdd)
+        {
+            int total = 0;
+
+            if (!String.IsNullOrWhiteSpace(numbersToAdd))
+            {
+                string[] splitNumbers = numbersToAdd.Split(',', '\n');
+
+                foreach (var item in splitNumbers)
+                {
+                    total += Convert.ToInt32(item);
+                }
+            }
+
+            return total;
+        }
+    }
+}
